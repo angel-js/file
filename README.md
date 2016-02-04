bafe94f73d2f0c6e23db392dbe322e98ed1bd1fa
Pau Fracés <pfraces@gmail.com>
2016-02-04 23:46:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+angel.file
+==========
+
+File utils for angel / angular 1.x
+
+Install
+-------
+
+    bower install ngl.file
+
+API
+---
+
+### `nglFileRead`
+
+```js
+nglFileRead(file).then(function (content) { ... });
+```
+
+  * **file:** a file from an `input[type="file"]`
+  * **content:** the file content URL-encoded
+
+### `nglFileReadImage`
+
+```js
+nglFileReadImage(file).then(function (image) { ... });
+```
+
+  * **file:** a file from an `input[type="file"]`
+  * **image** a `new window.Image()` instance
+
+References
+
+  * http://stackoverflow.com/questions/5173796/html5-get-image-dimension
+  * http://renevier.net/misc/resizeimg.html
+  * https://gist.github.com/pfraces/04889c82006f77864a4a
+
+### `<ngl-file-on-change>`
+
+Simple wrapper to `input[type=file].addEventListener('change')`
+
+  * must be placed on `input[type=file]` elements only
+  * doesn't emit event when selecting the same file multiple times in a row
+
+```html
+<input type="file" ngl-file-on-change="handleFileChange" />
+```
+
+```js
+$scope.handleFileChange = function (files) { ... };
+```
+
+References
+
+  * http://stackoverflow.com/a/19647381/1815446
+
+### `<ngl-file-on-select>`
+
+Higher-level abstraction to `input[type=file]` elements
+
+  * can't be placed on `input[type]` elements
+  * listen to `click` events
+  * allow selecting the same file multiple times in a row
+
+```html
+<button ngl-file-on-select="handleFileSelect">Select</button>
+```
+
+```js
+$scope.handleFileSelect = function (files) { ... }
+```
+
+References
+
+  * Use same file multiple times: http://stackoverflow.com/a/27346890/1815446
+
+### `<ngl-file-write>`
+
+```html
+<div ngl-file-write="lorem.txt" ngl-file-content="{{ content }}"></div>
+```
+
+```js
+$scope.content = 'lorem ipsum ...';
+```
+
+References
+
+  * Create a file from JavaScript: http://stackoverflow.com/a/20343999/1815446
+  * Save/download data generated in Javascript: http://hackworthy.blogspot.com.es/2012/05/savedownload-data-generated-in.html
---
+++ src/file.js
@@ -0,0 +1,120 @@
+angular.module('ngl.file', [])
+
+.factory('nglFileRead', function ($window, $q) {
+    'use strict';
+
+    var read = function (file) {
+        var deferred = $q.defer();
+        var reader = new $window.FileReader();
+
+        reader.onload = function (event) {
+            deferred.resolve(event.target.result);
+        };
+
+        reader.readAsDataURL(file);
+        return deferred.promise;
+    };
+
+    return read;
+})
+
+.factory('nglFileReadImage', function ($window, $q, nglFileRead) {
+    'use strict';
+
+    var readImage = function (file) {
+        var deferred = $q.defer();
+        var image = new $window.Image();
+
+        image.onload = function () { deferred.resolve(image); };
+        image.onerror = function () { deferred.reject(file); };
+
+        nglFileRead(file)
+        .then(function (content) { image.src = content; })
+        ['catch'](image.onerror);
+
+        return deferred.promise;
+    };
+
+    return readImage;
+})
+
+.directive('nglFileOnChange', function ($parse) {
+    'use strict';
+
+    var link = function (scope, element, attr) {
+        var callback = $parse(attr.nglFileOnChange)(scope);
+
+        element.on('change', function (event) {
+            var files = event.target.files;
+            if (!files.length) { return; }
+            
+            scope.$apply(function () {
+                callback(files);
+            });
+        });
+    };
+
+    return {
+        link: link
+    };
+})
+
+.directive('nglFileOnSelect', function ($parse) {
+    'use strict';
+
+    var tpl = [
+        '<form>',
+            '<input type="file" />',
+        '</form>'
+    ].join('');
+
+    var link = function (scope, element, attr) {
+        var form = angular.element(tpl);
+        var input = form.children('input');
+        var callback = $parse(attr.nglFileOnSelect)(scope);
+
+        input.on('change', function (event) {
+            var files = event.target.files;
+            if (!files.length) { return; }
+            
+            scope.$apply(function () {
+                callback(files);
+            });
+        });
+
+        element.on('click', function () {
+            form[0].reset();
+            input[0].click();
+        });
+    };
+
+    return {
+        link: link
+    };
+})
+
+.directive('nglFileWrite', function ($window, $document) {
+    'use strict';
+    
+    var tpl = [
+        '<a href="" download="">Download File</a>'
+    ].join('');
+
+    var jsonUrl = function (json) {
+        var url = $window.encodeURIComponent(json);
+        return 'data:text/json;charset=utf8,' + url;
+    };
+
+    var link = function (scope, element, attrs) {
+        var form = angular.element(tpl);
+        var anchor = form.children('a');
+        
+        attrs.$observe('nglFileWrite', function (filename) { anchor.attr({ download: filename }); });
+        attrs.$observe('nglFileContent', function (content) { anchor.attr({ href: jsonUrl(content) }); });
+        element.on('click', function () { anchor[0].click(); });
+    };
+
+    return {
+        link: link
+    };
+});
